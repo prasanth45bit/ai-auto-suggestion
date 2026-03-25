4652d0c464e933ad69896e020c0d17f0d3647daa
prasanth45bit <prasanthpalanisamy4@gmail.com>
2026-03-25 22:12:19 +0530

    update

--- app.py
+++ app.py
@@ -11,3 +11,14 @@ class InputText(BaseModel):
 def predict(data: InputText):
     words = predict_next_words(data.text)
     return {"suggestions": words}
+
+@app.get("/")
+def home():
+    return "Hello"
+
+if __name__ == "__main__": 
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+    
